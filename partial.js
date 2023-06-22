7691e8e2b1d14207da2ef81635479fa35419e0aa
Clément Ritz <clement.ritz@orange.fr>
2023-06-22 12:50:14 +0200

    first push

---
+++ partial.js
@@ -0,0 +1,9 @@
+// Chargement du contenu de la navbar
+$(function() {
+  $("#navbar-placeholder").load("navbar.html");
+});
+
+// Chargement du contenu du footer
+$(function() {
+  $("#footer-placeholder").load("footer.html");
+});
